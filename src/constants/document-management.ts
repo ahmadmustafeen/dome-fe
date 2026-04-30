3ef89b5ed496f5bc020ea04a6cb5a1da76fe381f
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2026-04-30 17:20:31 +0500

    feat: added new doc typs and removed old types

--- src/constants/document-management.ts
+++ src/constants/document-management.ts
@@ -4,9 +4,12 @@ export const DOCUMENT_TYPES: DocumentType[] = [
   'Asset Maintenance and Service Documents',
   'Asset Manuals and Diagrams',
   'Site Diagrams and Documents',
-  'Outdated MOPs, SOPs and EOPs',
+  // 'Outdated MOPs, SOPs and EOPs',
   'Company Policies and Documents',
-  'Asset List',
+  'Basis of Design',
+  'Electrical One-Line Diagram',
+  'Mechanical One-Line Diagram'
+  // 'Asset List',
 ];
 
 export const DOCUMENT_TYPE_BADGE: Record<DocumentType, string> = {
@@ -15,5 +18,8 @@ export const DOCUMENT_TYPE_BADGE: Record<DocumentType, string> = {
   'Site Diagrams and Documents': 'bg-green-100 text-green-700',
   'Outdated MOPs, SOPs and EOPs': 'bg-orange-100 text-orange-700',
   'Company Policies and Documents': 'bg-red-100 text-red-700',
+  'Basis of Design': 'bg-red-100 text-red-700',
+  'Electrical One-Line Diagram': 'bg-red-100 text-red-700',
+  'Mechanical One-Line Diagram': 'bg-red-100 text-red-700',
   'Asset List': 'bg-yellow-100 text-yellow-700',
 };

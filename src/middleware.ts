94b422c416930a8827125fcc5a7b31b44a04be88
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2026-05-02 02:17:34 +0500

    feat: removed additional middleware

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from "next-intl/middleware";
-
-import { routing } from "./libs/I18nRouting";
-
-export default createMiddleware(routing);
-
-export const config = {
-  matcher: ["/((?!api|_next|_next/static|_next/image|_vercel|.*\\..*).*)"],
-};
